547a03d1cb70eed4b45441a85dc64c0193f2a71a
briantmorr <briantmorr@gmail.com>
2018-03-29 14:37:59 -0700

    we can add items to our list!

--- src/components/list.js
+++ src/components/list.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import listData from '../data/todo_items';
-
-console.log('ListData: ', listData);
-
+// console.log('ListData:', listData);
 export default props => {
-    const listElements = listData.map((item, index) => {
-        return <li className="collection-item" key={index}>{item.title}</li>
+    const listElements = props.list.map((item, index) => {
+        return(
+            <li key={index} className='collection-item'>{item.title}</li>
+        );
     })
-    console.log(listElements);
-    return (
-        <ul className="collection">
-         {listElements}
-         {
-             [
-                <li className="collection-item" key='6'>test</li>,
-                <li className="collection-item" key='9'>zzz</li>
-             ]
-         }
+    return(
+        <ul className='collection'>
+            {listElements}
         </ul>
-    )
+    );
 }
