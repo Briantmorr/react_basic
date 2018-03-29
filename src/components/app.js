87e87a155b4fb19d811ed9b72520d2c3700228c0
briantmorr <briantmorr@gmail.com>
2018-03-29 12:13:41 -0700

    finished prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-
+import List from './list';
 
 const App = () => (
     <div className="container">
         <h1 className="center">To Do List</h1>
+        <List/>
     </div>
 );
 
