b0bd0fd904d8ed12ff99248890bb0a227c12c682
briantmorr <briantmorr@gmail.com>
2018-04-02 14:29:20 -0700

    connecting to api

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import axios from 'axios';
 import List from './list';
 import AddForm from './addForm';
 import listData from '../data/todo_items';
@@ -10,7 +11,36 @@ import {
 } from 'react-router-dom';
 import About from './about';
 
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=testuser1234';
+
+
 class App extends Component {
+    // constructor(props){
+    //     super(props);
+
+    //     this.state={
+    //         list:[]
+    //     }
+    // }
+    // componentDidMount(){
+    //     this.getList();
+    // }
+    // getList(){
+    //     axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+    //         this.setState({
+    //             list: resp.data.todos
+    //         });
+    //         console.log('response is: ', resp);
+    //         console.log('state is: ', this.state.list);
+    //     });
+    // }
+    // addItem(item){
+    //     axios.post(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+    //         console.log('response of add is: ', resp);
+    //         this.getList();
+    //     });
+    // }
     render() {
         return (
             <div className="container">
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import List from './list';
 import AddForm from './addForm';
@@ -7,17 +9,36 @@ import {
     Route,
     Link
 } from 'react-router-dom';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=testuser1234';
+
+
 class Home extends Component {
-    constructor(props) {
+        constructor(props){
         super(props);
-        this.state = {
-            list: listData
+
+        this.state={
+            list:[]
         }
     }
-    addItem(item) {
-        this.setState({
-            list: [item, ...this.state.list]
-        })
+    componentDidMount(){
+        this.getList();
+    }
+    getList(){
+        axios.get(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+            this.setState({
+                list: resp.data.todos
+            });
+            console.log('response is: ', resp);
+            console.log('state is: ', this.state.list);
+        });
+    }
+    addItem(item){
+        axios.post(`${BASE_URL}/todos${API_KEY}`).then(resp => {
+            console.log('response of add is: ', resp);
+            this.getList();
+        });
     }
     deleteItem(item){
         let newList = this.state.list.slice();
@@ -25,7 +46,19 @@ class Home extends Component {
         this.setState({
             list:newList
         })
+
+        //axios.delete
+        //ulr: http://api.reactprototypes.com/todos/[item id]?key=[your api-key]
     }
+    toggleComplete(){
+        //axios.put
+    
+    }
+    getOneItem(){
+        //axios.get
+        //url + id
+    }
+    
     render() {
         return (
             <div className="container">
